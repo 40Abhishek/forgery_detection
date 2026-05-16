18cfbb651bf8706d5846340853b12982c3cb3751
Bharat <swamibharat2110@gmail.com>
2026-05-16 19:44:38 +0530

    client:api.js fixed

--- stage0_web/client/src/api/api.js
+++ stage0_web/client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+  baseURL: "https://dtds-mm7w.onrender.com/api"
 });
 
 export const uploadFile = async (file) => {
